2996b825dfb84ecb3d372401ea60ad692ccb3a45
Vinicius Lacerda <vini.lacerda@hotmail.com>
2025-10-30 00:28:29 -0300

    feat: product delete on product controller

--- src/main/java/com/udemyJava/course/resources/ProductResource.java
+++ src/main/java/com/udemyJava/course/resources/ProductResource.java
@@ -4,10 +4,7 @@ import com.udemyJava.course.entities.Product;
 import com.udemyJava.course.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +27,10 @@ public class ProductResource {
     public ResponseEntity<Product> findById(@PathVariable Long id){
         Product obj = productService.findProductById(id);
         return ResponseEntity.ok().body(obj);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        productService.deletProductById(id);
+        return ResponseEntity.noContent().build();
     }
 }
