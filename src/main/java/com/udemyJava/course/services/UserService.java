e43c554e557518d29cd74da8ecafd554f90a1cc1
Vinicius Lacerda <vini.lacerda@hotmail.com>
2025-11-02 17:52:21 -0300

    feat: exception handling - delete

--- src/main/java/com/udemyJava/course/services/UserService.java
+++ src/main/java/com/udemyJava/course/services/UserService.java
@@ -2,10 +2,13 @@ package com.udemyJava.course.services;
 
 import com.udemyJava.course.entities.User;
 import com.udemyJava.course.repositories.UserRepository;
+import com.udemyJava.course.services.exceptions.DatabaseException;
 import com.udemyJava.course.services.exceptions.ResourceNotFoundException;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -29,7 +32,13 @@ public class UserService {
         return userRepository.save(user);
     }
     public void delete(Long id){
-        userRepository.deleteById(id);
+        try{
+            userRepository.deleteById(id);
+        }catch(EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
     public User update(Long id, User user){
         try {
